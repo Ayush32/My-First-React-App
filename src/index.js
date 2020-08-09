5030f94aca428cb1d4dc5ef26d4ba09af0b1ca33
Ayush Gupta <721ayush@gmail.com>
2020-08-09 19:32:15 +0530

    first commit to git repository

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/*
+ *   Copyright (c) 2020
+ *   All rights reserved.
+ */
+// Import the react and reactDOM libraries
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from "react";
+import ReactDOM from "react-dom";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Create a  react component
+
+const App = () => {
+  return <div>Hi my first react app</div>;
+};
+
+// take the react component and show it on the screen
+
+ReactDOM.render(<App />, document.querySelector("#root"));
