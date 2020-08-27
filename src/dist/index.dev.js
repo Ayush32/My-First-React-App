ed14996ccb90bb26bebf91d60737b3c1b841db0c
Ayush Gupta <721ayush@gmail.com>
2020-08-27 17:44:32 +0530

    create react dom to hsow on browser

--- src/dist/index.dev.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _react = _interopRequireDefault(require("react"));
-
-var _reactDom = _interopRequireDefault(require("react-dom"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
--- src/index.js
+++ src/index.js
@@ -8,5 +8,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // Create a react components
+const App = () => {
+  return <div>Hi This is my First React App</div>;
+};
 
 // take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
