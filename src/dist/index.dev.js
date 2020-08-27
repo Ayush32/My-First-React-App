ba8da44c16c29e46487f522181cfe399a54447d5
Ayush Gupta <721ayush@gmail.com>
2020-08-27 17:28:48 +0530

    import react

---
+++ src/dist/index.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+var _reactDom = _interopRequireDefault(require("react-dom"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
--- src/index.js
+++ src/index.js
@@ -2,44 +2,11 @@
  *   Copyright (c) 2020
  *   All rights reserved.
  */
-// Import the react and reactDOM libraries
 
+//  import the react and ReactDom libraries
 import React from "react";
 import ReactDOM from "react-dom";
 
-// Create a  react component
-
-const App = () => {
-  // js variable
-  //   const buttonText = "Click Me!";
-  const buttonText = {
-    text: "Click Me",
-  };
-  const labelText = "Enter Name:";
-  return (
-    <div>
-      <label className="label" htmlFor="name">
-        {labelText}
-      </label>
-      <input type="text" id="name" />
-      <br />
-      <label className="label" htmlFor="email">
-        Enter Email:
-      </label>
-      <input type="email"></input>
-      <button
-        style={{
-          backgroundColor: "blue",
-          color: "white",
-          borderRadius: "10px",
-        }}
-      >
-        {buttonText.text}
-      </button>
-    </div>
-  );
-};
+// Create a react components
 
 // take the react component and show it on the screen
-
-ReactDOM.render(<App />, document.querySelector("#root"));
