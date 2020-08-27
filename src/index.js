d5a9b02fe9b46ef3f25d7de6d5ee1e20ee067038
Ayush Gupta <721ayush@gmail.com>
2020-08-27 18:16:41 +0530

    styling with inline

--- src/index.js
+++ src/index.js
@@ -5,11 +5,31 @@
 
 //  import the react and ReactDom libraries
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom"; /* html - style=
+      background-color: "blue" */
 
+/* jsx - {{style= backgroundColor: "blue"}} */
 // Create a react components
 const App = () => {
-  return <div>Hi This is my First React App</div>;
+  return (
+    <div>
+      <label class="label" for="name">
+        Enter Name:
+      </label>
+      <input id="name" type="text" />
+
+      <button
+        style={{
+          backgroundColor: "blue",
+          border: "2px solid red",
+          borderRadius: "4px",
+          color: "white",
+        }}
+      >
+        Submit
+      </button>
+    </div>
+  );
 };
 
 // take the react component and show it on the screen
